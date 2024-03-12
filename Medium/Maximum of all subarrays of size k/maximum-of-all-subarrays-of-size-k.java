087f1ebd2ee2c6c32ff2916f5a52c99342e6fdba
tsrithvik <143204046+tsrithvik@users.noreply.github.com>
2024-03-12 18:03:20 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum of all subarrays of size k/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        ArrayList<Integer> max= new ArrayList<>();
+            //int sum=0;
+            int j=0;
+            while(j<=n-k){
+                int mx=0,sum=0;
+                for(int i=0;i<k;i++){
+                    //sum+= arr[i];
+                    sum= Math.max(mx,arr[i+j]);
+                    if(sum>mx) mx= sum; 
+                }
+                max.add(sum);
+                j++;
+            }
+            return max;
+    }
+}
