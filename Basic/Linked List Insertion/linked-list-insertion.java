1b3a77a24403a9eb5b31ab583242f3c21b3f3962
tsrithvik <143204046+tsrithvik@users.noreply.github.com>
2024-03-17 23:34:21 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Linked List Insertion/linked-list-insertion.java
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+    static void printList(Node node) 
+    { 
+        while (node != null) 
+        { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+        System.out.println(); 
+    }
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int n = sc.nextInt();
+            Node head = null;
+            for(int i=0; i<n; i++)
+            {
+                int value = sc.nextInt();
+                int indicator = sc.nextInt();
+
+                Solution ob = new Solution();
+                if(indicator == 0)
+                    head = ob.insertAtBeginning(head, value);
+                else
+                    head = ob.insertAtEnd(head, value);
+            }
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to insert a node at the beginning of the linked list.
+    Node insertAtBeginning(Node head, int x)
+    {
+        // code here
+        if(head==null) return new Node(x);
+        Node n= new Node(x);
+        n.next= head;
+        head= n;
+        return n;
+    }
+    
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+        // code here
+        
+        if(head==null) return new Node(x);
+        //Node n= new Node;
+        Node tem= head;
+        while(tem.next!=null){
+            tem=tem.next;
+          }  
+          
+                Node n= new Node(x);
+                tem.next= n;
+            
+        
+        return head;
+    }
+}
