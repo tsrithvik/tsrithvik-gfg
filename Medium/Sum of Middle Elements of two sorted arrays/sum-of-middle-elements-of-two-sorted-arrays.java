77b619a18d68c09d09f7940b74c51942616d36d5
tsrithvik <143204046+tsrithvik@users.noreply.github.com>
2024-05-17 20:10:16 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Sum of Middle Elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+  public class GFG {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] ar1 = new int[n], ar2 = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+
+            for (int i = 0; i < n; i++) {
+                ar1[i] = Integer.parseInt(inputLine[i]);
+            }
+            inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                ar2[i] = Integer.parseInt(inputLine[i]);
+            }
+            System.out.println(new Solution().findMidSum(ar1, ar2, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int findMidSum(int[] ar1, int[] ar2, int n) {
+        // code here
+        int[] a1= new int[2*n];
+        int k=0;
+        for(int i=0;i<n;i++){
+            a1[k++]= ar1[i];
+        }
+        for(int i=0;i<n;i++){
+            a1[k++]= ar2[i];
+        }
+        Arrays.sort(a1);
+        //int mid= n/2;
+        return a1[n]+a1[n-1];
+    }
+}
