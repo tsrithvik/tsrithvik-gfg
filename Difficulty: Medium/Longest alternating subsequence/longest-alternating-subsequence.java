e008fd90b51e0b1863cdd7e834f5264178f2f524
Sai Rithvik <143204046+tsrithvik@users.noreply.github.com>
2024-06-22 01:42:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest alternating subsequence/longest-alternating-subsequence.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] S = br.readLine().split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++)
+                nums[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.AlternatingaMaxLength(nums);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int AlternatingaMaxLength(int[] nums)
+    {
+        // code here
+        int inc=1, dec=1;
+        if(nums.length==1) return 1;
+        
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1]) inc= dec+1;
+            else if(nums[i]>nums[i+1]) dec= inc+1;
+        }
+        return Math.max(inc, dec);
+    }
+}
