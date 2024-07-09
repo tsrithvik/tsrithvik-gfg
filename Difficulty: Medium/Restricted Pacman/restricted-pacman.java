00f9efb5c9b6fc3fe05369d242adb53563412bb2
Sai Rithvik <143204046+tsrithvik@users.noreply.github.com>
2024-07-09 19:16:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Restricted Pacman/restricted-pacman.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+     public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t > 0){
+            int m = sc.nextInt();
+            int n = sc.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.candies(m,n));
+            t--;
+        }
+    } 
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int candies(int m, int n)
+    {
+        // Your code goes here 
+        return ((m-1)*(n-1))/2;
+    }
+}
