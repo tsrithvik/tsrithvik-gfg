96c6eea60beed360e8e60f4547c91187aec9c1d1
Sai Rithvik <143204046+tsrithvik@users.noreply.github.com>
2024-07-28 23:26:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Form a palindrome/form-a-palindrome.py
+++ Difficulty: Medium/Form a palindrome/form-a-palindrome.py
@@ -1,35 +1,30 @@
 #User function Template for python3
 
 class Solution:
-    def countMin(self, str):
+    def findMinInsertions(self, s):
         # code here
-            n=len(str)
-            dp= [[None]*(n+1) for _ in range(n+1)]
-            for i in range(n+1):
-                for j in range(n+1):
-                    if i==0 or j==0:
-                        dp[i][j]=0
+        n=len(s)
+        dp= [[None]*(n+1) for _ in range(n+1)]
+        for i in range(n+1):
+            for j in range(n+1):
+                if i==0 or j==0:
+                    dp[i][j]=0
+                else:
+                    if s[i-1]==s[n-j]:
+                        dp[i][j]=1+dp[i-1][j-1]
                     else:
-                        if str[i-1]==str[n-j]:
-                            dp[i][j]=1+dp[i-1][j-1]
-                        else:
-                            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-            return n-dp[n][n]
-
+                        dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        return n-dp[n][n]
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 
-if __name__ == '__main__':
-
-    t = int(input())
-
+if __name__=='__main__':
+    t=int(input())
     for _ in range(t):
-        Str = input()
-
-        solObj = Solution()
-
-        print(solObj.countMin(Str))
-
+        
+        S = input().strip()
+        ob=Solution()
+        print(ob.findMinInsertions(S))
 # } Driver Code Ends
