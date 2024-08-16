0bb55387d75d1991b4ef743b388c8ec5bcd79520
Sai Rithvik <143204046+tsrithvik@users.noreply.github.com>
2024-08-17 02:46:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Recursively remove all adjacent duplicates/recursively-remove-all-adjacent-duplicates.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    string rremove(string s) {
+        // code here
+         string res = "";
+        int n = s.length();
+        
+        int i=0;
+        while(i<n){
+            char c = s[i];
+            int freq = 0;
+            while(s[i] == c)
+                freq++, i++;
+            
+            if(freq == 1)
+                res += c;
+        }
+        
+        if(res == s)
+            return res;
+        return rremove(res);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Solution ob;
+        cout << ob.rremove(s) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
