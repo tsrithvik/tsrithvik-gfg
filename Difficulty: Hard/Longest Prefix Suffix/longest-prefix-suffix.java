5fc910028ff28cd3539c3a4b2b9dac105db0f63a
Sai Rithvik <143204046+tsrithvik@users.noreply.github.com>
2024-09-22 10:45:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
+++ Difficulty: Hard/Longest Prefix Suffix/longest-prefix-suffix.java
@@ -1,16 +1,15 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 import java.io.*;
 import java.util.*;
 
 class GFG {
     public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(read.readLine());
         while (t-- > 0) {
-            
+
             String s = read.readLine();
             Solution ob = new Solution();
             System.out.println(ob.lps(s));
@@ -20,25 +19,27 @@ class GFG {
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
 class Solution {
-    int lps(String s) {
+    int lps(String str) {
         // code here
-        int i=0,j=1,k=1,cnt=0,n=s.length();
-        while(i<n-1 && j<n){
-            if(s.charAt(i)==s.charAt(j)){
-                cnt++;
+        int n= str.length();
+        int i=0, k=0, j=1, c=0;
+        while(i<n && j<n){
+            if(str.charAt(i)==str.charAt(j)){
                 i++;
                 j++;
+                c++;
             }
-            else {
+            else{
                 i=0;
                 k++;
-                cnt=0;
                 j=k;
+                c=0;
             }
         }
-        return cnt;
+        
+        return c;
     }
 }
