a632d48bf58316bad7aed0c0d6da79a2ef66d96b
Sai Rithvik <143204046+tsrithvik@users.noreply.github.com>
2024-09-29 09:00:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Total count/total-count.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    int totalCount(int k, int[] arr) {
+        // code here
+        
+        if(k<=0) return -1;
+        if(k==1) return Arrays.stream(arr).sum();
+        int count=0;
+        for(int i:arr){
+            count+= division(i, k);
+        }
+        
+        return count;
+    }
+    static int division(int n, int k){
+        if(n==0) return 0;
+        int c=0;
+        while(n>0 && k>=0){
+            if(n-k>=0){
+                n-=k;
+                c++;
+            }
+            else{
+                k--;
+            }
+            
+        }
+        
+        return c;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.totalCount(k, arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
